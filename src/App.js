9f813a57b77fa8bb55409d5423e90c7b37a8630a
tuschuk <tristen.uschuk21@gmail.com>
2025-04-08 20:05:01 -0600

    set up template for squat

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data = {
+    "squat": {
+      "name": "squat",
+      "guide": "this is a step by step on how to preform a squat",
+      "image": "/Squat.jpg",
+      "steps": {
+        1: "step 1",
+        2: "step 2",
+        3: "step 3",
+      }
+
+    }
+  }
+  
+  function displaySteps() {
+    const keys = Object.keys(data.squat.steps)
+
+    return keys.map(step => {
+      return <p>{data.squat.steps[step]}</p>
+    });
+  
+  } 
+
+  console.log(data.squat);
+  console.log(data.squat.guide);
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { data.squat.guide }
+          
         </p>
         <a
           className="App-link"
@@ -15,11 +41,39 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SBD Coaching Companion
         </a>
-      </header>
+      </header> */}
+      <section>
+        <h1>
+          {data.squat.name}
+        </h1>
+        <p>
+          {data.squat.guide}
+        </p>
+        <img src={data.squat.image} className="image">
+        
+        </img>
+        <p>STEPS</p>
+        {
+          displaySteps()
+        }
+        {/* <p>{data.squat.steps[1]}</p>
+        <p>{data.squat.steps[2]}</p>
+        <p>{data.squat.steps[3]}</p> */}
+      </section>
+
+
+
+
+
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
