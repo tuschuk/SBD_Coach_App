b7299e9e4b7511913640eb5f714b8c49aa779d20
tuschuk <tristen.uschuk21@gmail.com>
2025-04-08 20:31:33 -0600

    Create Welcome Page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,29 @@ function App() {
         2: "step 2",
         3: "step 3",
       }
+    },
+
+    "deadlift": {
+      "name": "deadlift",
+      "guide": "this is a step by step on how to preform a deadlift",
+      "image": "/Deadlift.jpg",
+      "steps": {
+        1: "step 1",
+        2: "step 2",
+        3: "step 3",
+      }
+
+    },
+
+    "bench": {
+      "name": "bench",
+      "guide": "this is a step by step on how to preform a benchpress",
+      "image": "/Bench.jpg",
+      "steps": {
+        1: "step 1",
+        2: "step 2",
+        3: "step 3",
+      }
 
     }
   }
@@ -25,43 +48,39 @@ function App() {
   
   } 
 
-  console.log(data.squat);
-  console.log(data.squat.guide);
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          { data.squat.guide }
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          SBD Coaching Companion
-        </a>
-      </header> */}
-      <section>
+  function displayExercises() {
+    const keys = Object.keys(data);
+
+    return keys.map((item, index) => {
+      return (
+        <section key={index}>
         <h1>
-          {data.squat.name}
+          {data[item].name}
         </h1>
-        <p>
+        {/* <p>
           {data.squat.guide}
-        </p>
-        <img src={data.squat.image} className="image">
+        </p> */}
+        <img src={data[item].image} className="image">
         
         </img>
-        <p>STEPS</p>
+        {/* <p>STEPS</p>
         {
           displaySteps()
-        }
+        } */}
         {/* <p>{data.squat.steps[1]}</p>
         <p>{data.squat.steps[2]}</p>
         <p>{data.squat.steps[3]}</p> */}
       </section>
+      )
+    })
+  }
+
+  return (
+    <div className="exercises">
+      
+
+      {displayExercises()}
+
 
 
 
